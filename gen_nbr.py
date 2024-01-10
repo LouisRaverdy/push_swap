4ae61d22a580d433480319f4fe0529ceb7df2c99
Louis <louisraverdy@gmail.com>
2024-01-10 18:48:33 +0100

    Delete gen_nbr.py

--- gen_nbr.py
+++
@@ -1,9 +0,0 @@
-from random import shuffle
-user_input = input("Enter a number: ")
-try:
-    number = int(user_input)
-    l = list(range(1, number + 1))
-    shuffle(l)
-    print("Shuffled list:", " ".join([str(x) for x in l]))
-except ValueError:
-    print("Error: Please enter a valid integer.")
